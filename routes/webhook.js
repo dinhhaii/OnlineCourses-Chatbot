836c7463f27c27013781e91e13bfef93186e9e43
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 16:47:21 +0700

    Update code

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,30 +8,37 @@ router.get("/", (req, res) => {
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-    if (token === VERIFY_TOKEN) {
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  
+  if (token === VERIFY_TOKEN) {
+    console.log("WEBHOOK_VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 router.post("/webhook", (req, res) => {
   let body = req.body;
-    body.entry.forEach(function (entry) {
-      let event = entry.messaging[0];
-      let sender_psid = event.sender.id;
-      if (event.message) {
-        console.log(event.message);
-        handleMessage(sender_psid, event.message);
-      } else {
-        handlePostback(sender_psid, event.postback);
-      }
-    });
+  try {
+    if (body.object === "page") {
+      body.entry.forEach(function (entry) {
+        let event = entry.messaging[0];
+        let sender_psid = event.sender.id;
+        if (event.message) {
+          console.log(event.message);
+          handleMessage(sender_psid, event.message);
+        } else {
+          handlePostback(sender_psid, event.postback);
+        }
+      });
 
-    res.status(200).send("EVENT_RECEIVED");
-  
+      res.status(200).send("EVENT_RECEIVED");
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (e) {
+    console.log(e);
+    res.send(e.message);
+  }
 });
 
 module.exports = router;
