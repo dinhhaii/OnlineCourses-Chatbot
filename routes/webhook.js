0ba761e2d6d451a22f04d0d1b62737798037c320
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 18:49:56 +0700

    Update code

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,18 +1,20 @@
 var express = require("express");
 var router = express.Router();
 const { VERIFY_TOKEN } = require("../utils/constant");
-const { handleMessage, handlePostback } = require("../services/webhook");
+const services = require("../services/webhook");
 
 router.get("/", (req, res) => {
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-  if (token === VERIFY_TOKEN) {
-    console.log("WEBHOOK_VERIFIED");
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+  if (mode && token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
   }
 });
 
@@ -25,9 +27,9 @@ router.post("/", (req, res) => {
         let sender_psid = event.sender.id;
         if (event.message) {
           console.log(event.message);
-          handleMessage(sender_psid, event.message);
+          services.handleMessage(sender_psid, event.message);
         } else {
-          handlePostback(sender_psid, event.postback);
+          services.handlePostback(sender_psid, event.postback);
         }
       });
 
