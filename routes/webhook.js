e3605506eb642d72f291202c97571820d7c07493
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 16:40:22 +0700

    Update code

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,14 +8,13 @@ router.get("/", (req, res) => {
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
 
-  if (mode && token) {
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    if (token === VERIFY_TOKEN) {
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
       res.sendStatus(403);
     }
-  }
+  
 });
 
 router.post("/webhook", (req, res) => {
