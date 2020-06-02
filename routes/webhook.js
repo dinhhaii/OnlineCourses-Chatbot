bf09f4b7670a8afb2f17d11e5333e7d95e0b59bb
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 16:05:17 +0700

    Fix webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,44 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { VERIFY_TOKEN } = require('../utils/constant');
-const { handleMessage, handlePostback } = require('../services/webhook');
+const { VERIFY_TOKEN } = require("../utils/constant");
+const { handleMessage, handlePostback } = require("../services/webhook");
 
 router.get("/", (req, res) => {
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
   if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('WEBHOOK_VERIFIED');
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
-      res.sendStatus(403);      
+      res.sendStatus(403);
     }
   }
 });
 
 router.post("/webhook", (req, res) => {
   let body = req.body;
-  console.log(body);
-  if (body.object === 'page') {
-    body.entry.forEach(function(entry) {
-      let event = entry.messaging[0];
-      let sender_psid = event.sender.id;
-      if (event.message) {
-        console.log(event.message);
-        handleMessage(sender_psid, event.message); 
-      } else {
-        handlePostback(sender_psid, event.postback);
-      }
+  body.entry.forEach(function (entry) {
+    let event = entry.messaging[0];
+    let sender_psid = event.sender.id;
+    if (event.message) {
+      console.log(event.message);
+      handleMessage(sender_psid, event.message);
+    } else {
+      handlePostback(sender_psid, event.postback);
+    }
+  });
 
-    });
-
-    res.status(200).send('EVENT_RECEIVED');
-
-  } else {
-    res.sendStatus(404);
-  }
-})
+  res.status(200).send("EVENT_RECEIVED");
+});
 
 module.exports = router;
