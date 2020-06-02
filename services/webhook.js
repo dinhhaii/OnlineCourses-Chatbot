86f1333e4ccff0330a0d26a1724bb70509a71f1b
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 19:00:29 +0700

    Update code

--- services/webhook.js
+++ services/webhook.js
@@ -1,86 +1,84 @@
 const request = require("request");
 const PAGE_ACCESS_TOKEN = require("../utils/constant");
 
-module.exports = {
-  callSendAPI: function (sender_psid, response) {
-    let request_body = {
-      recipient: {
-        id: sender_psid,
-      },
-      message: response,
-    };
+const callSendAPI = (sender_psid, response) => {
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
 
-    request(
-      {
-        url: "https://graph.facebook.com/v2.6/me/messages",
-        qs: {
-          access_token: process.env.PAGE_ACCESS_TOKEN || PAGE_ACCESS_TOKEN,
-        },
-        method: "POST",
-        json: request_body,
+  request(
+    {
+      url: "https://graph.facebook.com/v2.6/me/messages",
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN || PAGE_ACCESS_TOKEN,
       },
-      (err, res, body) => {
-        if (!err) {
-          console.log("Message sent!");
-        } else {
-          console.error("Unable to send message:" + err);
-        }
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("Message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
       }
-    );
-  },
+    }
+  );
+};
 
-  handleMessage: function (sender_psid, received_message) {
-    let response;
+export const handleMessage = (sender_psid, received_message) => {
+  let response;
 
-    if (received_message.text) {
-      response = {
-        text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
-      };
-    } else if (received_message.attachments) {
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [
-              {
-                title: "Is this the right picture?",
-                subtitle: "Tap a button to answer.",
-                image_url: attachment_url,
-                buttons: [
-                  {
-                    type: "postback",
-                    title: "Yes!",
-                    payload: "yes",
-                  },
-                  {
-                    type: "postback",
-                    title: "No!",
-                    payload: "no",
-                  },
-                ],
-              },
-            ],
-          },
+  if (received_message.text) {
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an attachment!`,
+    };
+  } else if (received_message.attachments) {
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+            {
+              title: "Is this the right picture?",
+              subtitle: "Tap a button to answer.",
+              image_url: attachment_url,
+              buttons: [
+                {
+                  type: "postback",
+                  title: "Yes!",
+                  payload: "yes",
+                },
+                {
+                  type: "postback",
+                  title: "No!",
+                  payload: "no",
+                },
+              ],
+            },
+          ],
         },
-      };
-    }
+      },
+    };
+  }
 
-    callSendAPI(sender_psid, response);
-  },
+  callSendAPI(sender_psid, response);
+};
 
-  handlePostback: function (sender_psid, received_postback) {
-    let response;
+export const handlePostback = (sender_psid, received_postback) => {
+  let response;
 
-    let payload = received_postback.payload;
+  let payload = received_postback.payload;
 
-    if (payload === "yes") {
-      response = { text: "Thanks!" };
-    } else if (payload === "no") {
-      response = { text: "Oops, try sending another image." };
-    }
+  if (payload === "yes") {
+    response = { text: "Thanks!" };
+  } else if (payload === "no") {
+    response = { text: "Oops, try sending another image." };
+  }
 
-    callSendAPI(sender_psid, response);
-  },
+  callSendAPI(sender_psid, response);
 };
