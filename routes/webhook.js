a2487ee9c177071731e2c70b9a244faf7a3f7d9f
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 14:06:25 +0700

    Update webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,7 +3,10 @@ var router = express.Router();
 const constant = require('../utils/constant');
 
 router.get("/", (req, res) => {
-  res.send('Hello Hai');
+  if (req.query['hub.verify_token'] === "hacademy-chatbot") {
+      res.send(req.query['hub.challenge']);
+  }
+  res.send("Wrong token");
 });
 
 module.exports = router;
