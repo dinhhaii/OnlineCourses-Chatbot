e0281be1797019799db9ed54c889d833ddbb5f56
Dinh Hai <dinhhhaii@gmail.com>
2020-06-02 15:04:47 +0700

    Update access token

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PAGE_ACCESS_TOKEN = EAAEJXbRQ95wBALO2qEnFrMca1bMKrOZA4HG6lz7BZAAdx6na4qLJzRREqZBjBny0edBnNstaAulcXvok60Om2d5mVedL0g1Yb6LPeXMeZBh10XZCsXq6qpRVor6Kand28cWNpD5RgbniryR5fgSiREjBjRuBXU2Naq9aAucd8tehOpG5T6h3x,
+    PAGE_ACCESS_TOKEN: EAAEJXbRQ95wBALO2qEnFrMca1bMKrOZA4HG6lz7BZAAdx6na4qLJzRREqZBjBny0edBnNstaAulcXvok60Om2d5mVedL0g1Yb6LPeXMeZBh10XZCsXq6qpRVor6Kand28cWNpD5RgbniryR5fgSiREjBjRuBXU2Naq9aAucd8tehOpG5T6h3x,
     VERIFY_TOKEN: "hacademy-chatbot",
     URL_CLIENT: "http://localhost:3001",
     URL_ADMIN: "http://localhost:3002",
