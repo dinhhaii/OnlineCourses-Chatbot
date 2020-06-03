6b167609cab6453f0773ed5e6a7d7941aa42eb97
Dinh Hai <dinhhhaii@gmail.com>
2020-06-03 19:06:51 +0700

    Update code

--- services/webhook.js
+++ services/webhook.js
@@ -69,15 +69,16 @@ function handleMessage(sender_psid, received_message) {
 
 function handlePostback(sender_psid, received_postback) {
   let response;
-  let payload = received_postback.payload;
-  
-  if (payload === "yes") {
-    response = { text: "Thanks!" };
-  } else if (payload === "no") {
-    response = { text: "Oops, try sending another image." };
+  let { payload } = received_postback;
+  if (payload) {
+    if (payload === "yes") {
+      response = { text: "Thanks!" };
+    } else if (payload === "no") {
+      response = { text: "Oops, try sending another image." };
+    }
+    callSendAPI(sender_psid, response);
   }
 
-  callSendAPI(sender_psid, response);
 }
 
 module.exports = { handleMessage, handlePostback };
