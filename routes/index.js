a7a5a26aa8c56b2898a3974db8164995fff7125a
Dinh Hai <dinhhhaii@gmail.com>
2020-06-05 14:36:31 +0700

    Update new version

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const constant = require('../utils/constant');
-
-router.get("/", (req, res) => {
-  res.send(`<a href=${constant.URL_CLIENT}>${constant.URL_CLIENT}</a>`);
-});
-
-module.exports = router;
--- utils/constant.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PAGE_ACCESS_TOKEN: "EAAEJXbRQ95wBALO2qEnFrMca1bMKrOZA4HG6lz7BZAAdx6na4qLJzRREqZBjBny0edBnNstaAulcXvok60Om2d5mVedL0g1Yb6LPeXMeZBh10XZCsXq6qpRVor6Kand28cWNpD5RgbniryR5fgSiREjBjRuBXU2Naq9aAucd8tehOpG5T6h3x",
-    VERIFY_TOKEN: "hacademy-chatbot",
-    URL_CLIENT: "http://localhost:3001",
-    URL_ADMIN: "http://localhost:3002",
-    // URL_CLIENT: "https://cafocc.web.app",
-    // URL_ADMIN: "https://cafocc-cms.web.app",
-}
